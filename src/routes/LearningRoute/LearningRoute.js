53cdf781ccb1f472ecba5e4c939b336eadf34438
nevelson <nevelson92@gmail.com>
2020-06-23 16:48:13 -0400

    More learning

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -4,26 +4,37 @@ import LanguageApiService from "../../services/language-api-service";
 
 class LearningRoute extends Component {
   static contextType = LanguageContext;
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialized: false,
+    };
+  }
 
   componentDidMount() {
     this.context.clearError();
 
-    LanguageApiService.getHead().then((res) => this.context.setHead(res));
-    LanguageApiService.getLanguage()
+    const promise1 = LanguageApiService.getHead().then((res) =>
+      this.context.setHead(res)
+    );
+    const promise2 = LanguageApiService.getLanguage()
       .then((res) => {
         this.context.setWordList(res.words);
         return this.context.setLanguage(res.language);
       })
       .catch((err) => this.context.setError(err));
+
+    Promise.all([promise1, promise2]).then(
+      this.setState({ initialized: true })
+    );
   }
 
-  renderLearningPage(words, head) {
-    // console.log("TESTING 1", head);
-    // console.log('TESTING 2', words);
-  }
+  renderLearningPage() {
+    const { error, words, head, language } = this.context;
+    console.log("TESTING 1", head);
+    console.log("TESTING 2", words);
+    console.log("TESTING 3", language);
 
-  render() {
-    const { error, words, head } = this.context;
     return (
       <main>
         <h2>Learning Page</h2>
@@ -31,18 +42,23 @@ class LearningRoute extends Component {
           {error ? (
             <p>There was and error, try again</p>
           ) : words ? (
-            this.renderLearningPage(words, head)
+            <section>
+              <p>Translate: {head.nextWord}</p>
+              <p>Total score: {language.total_score}</p>
+              {/* TO DO: PUT IN AN EVENT HANDLER ON FORM */}
+              <form>
+                <label className="basic-label">
+                  Name:{" "}
+                  <input
+                    className="basic-input"
+                    type="text"
+                    name="learn-guess-input"
+                  />
+                </label>
+                <input className="submit-btn" type="submit" value="Submit" />
+              </form>
+            </section>
           ) : null}
-
-          {/* 
-          //Write code to request GET /api/language/head > Service
-          //display the next word the user must submit their answer for
-          //Display the current total score
-          //Display the form to submit the next answer
-              an input#learn-guess-input,
-              a label for the input,
-              and a button[type=submit]. You don't need to implement this button's event handler yet, that's the next user story.
-        */}
 
           {/* 
         Part 2:
@@ -55,6 +71,10 @@ class LearningRoute extends Component {
         </section>
       </main>
     );
+  }
+
+  render() {
+    return <> {this.state.initialized ? this.renderLearningPage() : null}</>;
   }
 }
 
