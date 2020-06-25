04b4896ed742b6e23932adbf2066b2c61a83623e
nevelson <nevelson92@gmail.com>
2020-06-25 18:19:29 -0400

    Final fixes

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -7,51 +7,51 @@ import TokenService from "../../services/token-service";
 
 class LearningRoute extends Component {
   static contextType = LearningContext;
-  state = { error: null }
+  state = { error: null };
   // componentDidMount() {
   //   this.getNextWord(this.context, this.props.history);
   // }
   componentDidMount() {
-    this.context.reset()
+    this.context.reset();
 
     LanguageApiService.getHead()
-      .then(head => {
-        this.context.setTotalScore(head.totalScore)
-        this.context.setWordCorrectCount(head.wordCorrectCount)
-        this.context.setWordIncorrectCount(head.wordIncorrectCount)
-        this.context.setNextWord(head.nextWord)
+      .then((head) => {
+        this.context.setTotalScore(head.totalScore);
+        this.context.setWordCorrectCount(head.wordCorrectCount);
+        this.context.setWordIncorrectCount(head.wordIncorrectCount);
+        this.context.setNextWord(head.nextWord);
       })
-      .catch(error => {
-        if (error.error === 'Unauthorized request') {
-          TokenService.clearAuthToken()
-          this.props.history.push('/login')
+      .catch((error) => {
+        if (error.error === "Unauthorized request") {
+          TokenService.clearAuthToken();
+          this.props.history.push("/login");
         }
-        this.context.setError(error)
-      })
+        this.context.setError(error);
+      });
   }
 
-  onSubmit = ev => {
-    ev.preventDefault()
-    const { guess } = ev.target
+  onSubmit = (ev) => {
+    ev.preventDefault();
+    const { guess } = ev.target;
 
-    this.context.setGuess(guess.value)
+    this.context.setGuess(guess.value);
 
     LanguageApiService.postGuess(guess.value)
-      .then(head => {
-        this.context.setPrevWord(this.context.nextWord)
-        this.context.setTotalScore(head.totalScore)
-        this.context.setWordCorrectCount(head.wordCorrectCount)
-        this.context.setWordIncorrectCount(head.wordIncorrectCount)
-        this.context.setNextWord(head.nextWord)
-        this.context.setIsCorrect(head.isCorrect)
-        this.context.setAnswer(head.answer)
-        guess.value = ''
+      .then((head) => {
+        this.context.setPrevWord(this.context.nextWord);
+        this.context.setTotalScore(head.totalScore);
+        this.context.setWordCorrectCount(head.wordCorrectCount);
+        this.context.setWordIncorrectCount(head.wordIncorrectCount);
+        this.context.setNextWord(head.nextWord);
+        this.context.setIsCorrect(head.isCorrect);
+        this.context.setAnswer(head.answer);
+        guess.value = "";
       })
-      .catch(res => {
-        this.setState({ error: res.error })
-      })
-  }
-  
+      .catch((res) => {
+        this.setState({ error: res.error });
+      });
+  };
+
   // onSubmit(e, context) {
   //   e.preventDefault();
   //   const value = e.target.guessInput.value;
@@ -72,8 +72,8 @@ class LearningRoute extends Component {
   // }
 
   getNextWord = () => {
-    this.context.setIsCorrect(null)
-  }
+    this.context.setIsCorrect(null);
+  };
 
   // getNextWord(context, history) {
   //   context.reset();
@@ -119,114 +119,87 @@ class LearningRoute extends Component {
   //   );
   // }
   renderForm() {
-    const { error } = this.state
-    const { nextWord } = this.context
-    return <>
-      <section>
-        <div>
-          <h2>
-            Translate the word:
-          </h2>
-          <span>
-            {nextWord}
-          </span>
-        </div>
-      </section>
-      <section>
-        <form
-          className='LearnWordForm'
-          onSubmit={this.onSubmit}
-        >
+    const { error } = this.state;
+    const { nextWord } = this.context;
+    return (
+      <>
+        <section>
           <div>
-            {error && <p className='red'>{error}</p>}
+            <h2>Translate the word:</h2>
+            <span>{nextWord}</span>
           </div>
-          <div className='guess-input'>
-            <label htmlFor='learn-guess-input'>
-              What's the translation for this word?
-            </label>
-            <input
-              id='learn-guess-input'
-              name='guess'
-              required
-              placeholder='translation'
-              type='text'
-            />
-          </div>
-          <button type='submit'>
-            Submit your answer
-          </button>
-        </form>
-      </section>
-    </>
+        </section>
+        <section>
+          <form className="LearnWordForm" onSubmit={this.onSubmit}>
+            <div>{error && <p className="red">{error}</p>}</div>
+            <div className="guess-input">
+              <label htmlFor="learn-guess-input">
+                What's the translation for this word?
+              </label>
+              <input
+                id="learn-guess-input"
+                name="guess"
+                required
+                placeholder="translation"
+                type="text"
+              />
+            </div>
+            <button type="submit">Submit your answer</button>
+          </form>
+        </section>
+      </>
+    );
   }
 
   renderFeedback() {
-    const { isCorrect, prevWord, answer, guess } = this.context
+    const { isCorrect, prevWord, answer, guess } = this.context;
     return (
       <section>
-        <div>
-          {isCorrect
-            ? <h2 className='DisplayFeedback__correct'>
-                You were correct! :D
-              </h2>
-            : <h2 className='DisplayFeedback__incorrect'>
-                Good try, but not quite right :(
-              </h2>}
+        <div className="DisplayFeedback">
+          {isCorrect ? (
+            <h2 className="DisplayFeedback__correct">You were correct! :D</h2>
+          ) : (
+            <h2 className="DisplayFeedback__incorrect">
+              Good try, but not quite right :(
+            </h2>
+          )}
           <p>
-            The correct translation for{' '}
-            <span>
-              {prevWord}
-            </span>
-            was
-            <span>
-              {answer}
-            </span>
-            <br/>
-            and you chose{' '}
-            <span>
-              {guess}
-            </span>!
+            The correct translation for <span>{prevWord}</span> was{" "}
+            <span>{answer}</span> and you chose <span>{guess}</span>!
           </p>
-          <button
-            onClick={this.getNextWord}>
-            Try another word!
-          </button>
+          <button onClick={this.getNextWord}>Try another word!</button>
         </div>
       </section>
-    )
+    );
   }
 
   render() {
-    const showForm = this.context.isCorrect == null
+    const showForm = this.context.isCorrect == null;
     return (
-      <div className='LearningRoute'>
+      <div className="LearningRoute">
         <section>
-          <div className='DisplayScore'>
+          <div className="DisplayScore">
             <p>
-              Your total score is:{' '}
-              <span>
-                {this.context.totalScore}
-              </span>
+              Your total score is: <span>{this.context.totalScore}</span>
             </p>
           </div>
         </section>
         {showForm && this.renderForm()}
         {!showForm && this.renderFeedback()}
-        {showForm && <footer>
-          <div>
-          <p className='correct'>
-            You have answered this word correctly{' '}
-            <span>{this.context.wordCorrectCount}</span>{' '}
-            times.
-          </p>
-          <p className='incorrect'>
-            You have answered this word incorrectly{' '}
-            <span>{this.context.wordIncorrectCount}</span>{' '}
-            times.
-          </p>
-        </div>
-        </footer>
-      }
+        {showForm && (
+          <footer>
+            <div>
+              <p className="correct">
+                You have answered this word correctly{" "}
+                <span>{this.context.wordCorrectCount}</span> times.
+              </p>
+              <p className="incorrect">
+                You have answered this word incorrectly{" "}
+                <span>{this.context.wordIncorrectCount}</span> times.
+              </p>
+            </div>
+          </footer>
+        )}
       </div>
     );
   }
